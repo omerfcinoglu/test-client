f26eb0911bc8c9a1b301067b559be4e160411041
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-06-05 15:59:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import IndexPage from "@/pages/index";
 import ProjectDetails from "./pages/HomeContent/ProjectDetails";
 import AdminLogin from "./components/AdminDashboard/AdminLogin";
@@ -11,22 +11,20 @@ import VideoRequestForm from "./pages/HomeContent/VideoRequest";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/project" element={<ProjectDetails />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/videoRequest" element={<VideoRequestForm />} />
-        <Route path="/pages/:pageId" element={<CreatedPages />} />
-        <Route path="/posts/:postId" element={<PostDetail />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        </Route>
-        {/* Geçersiz URL’ler için fallback */}
-        <Route path="*" element={<IndexPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route path="/project" element={<ProjectDetails />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/videoRequest" element={<VideoRequestForm />} />
+      <Route path="/pages/:pageId" element={<CreatedPages />} />
+      <Route path="/posts/:postId" element={<PostDetail />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Route>
+      {/* Geçersiz URL’ler için fallback */}
+      <Route path="*" element={<IndexPage />} />
+    </Routes>
   );
 }
 
