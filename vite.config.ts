824b5387651a791937e888a332003434d0c3f2e6
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-06-05 16:05:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: './',
+  base: "/",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
   },
   optimizeDeps: {
-    include: ['@heroui/react', '@heroui/button']
-  }
-})
+    include: ["@heroui/react", "@heroui/button"],
+  },
+});
